8c239c5712c3f5ef627dd499a069c7a73e1c4abc
chrisdavidmills <chrisdavidmills@gmail.com>
2018-02-12 13:15:16 +0000

    repo population

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* Challenge */
+router.get('/cool', function(req, res, next) {
+  res.send('You\'re so cool!');
+});
+
+module.exports = router;
